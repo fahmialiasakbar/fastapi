6163319a404ee91e3bf5513163be06e38603c856
Fahmi Akbar <fahmialiasakbar@gmail.com>
2022-04-30 06:53:05 +0700

    HTTP Get Requests

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from uuid import uuid4
+from uuid import uuid4, UUID
 from typing import List
 from fastapi import FastAPI
 from models import User, Gender, Role
@@ -6,18 +6,18 @@ app = FastAPI()
 
 db: List[User] = [
     User(
-        id = uuid4(),
-        fist_name = "Fahmi",
-        last_name = "Fakbar",
-        gender = Gender.male,
-        roles = [Role.student]
+        id=UUID("9e72bccb-6e65-4342-bfa9-f18046abfed8"),
+        first_name="Fahmi",
+        last_name="Fakbar",
+        gender=Gender.male,
+        roles=[Role.student]
     ),
     User(
-        id = uuid4(),
-        first_name = "Jack",
-        last_name = "Millder",
-        gender = Gender.male,
-        roles = [Role.admin, Role.user]
+        id=UUID("c85f70b8-0516-48a0-805c-9f03ed9dba57"),
+        first_name="Jack",
+        last_name="Millder",
+        gender=Gender.male,
+        roles=[Role.admin, Role.user]
     )
 ]
 
@@ -27,4 +27,7 @@ async def root():
         "Hello" : "World"
     }
 
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db;
 
