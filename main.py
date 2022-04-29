21125040234de7a791dd279e10a96e6c5a3fbaa2
Fahmi Akbar <fahmialiasakbar@gmail.com>
2022-04-30 06:47:15 +0700

    Database

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
+from uuid import uuid4
+from typing import List
 from fastapi import FastAPI
-
+from models import User, Gender, Role
 app = FastAPI()
+
+db: List[User] = [
+    User(
+        id = uuid4(),
+        fist_name = "Fahmi",
+        last_name = "Fakbar",
+        gender = Gender.male,
+        roles = [Role.student]
+    ),
+    User(
+        id = uuid4(),
+        first_name = "Jack",
+        last_name = "Millder",
+        gender = Gender.male,
+        roles = [Role.admin, Role.user]
+    )
+]
 
 @app.get("/")
 async def root():
     return {
         "Hello" : "World"
     }
+
 
