607bf4d871dc8731d7c1b193b0784b39464b9c72
Fahmi Akbar <fahmialiasakbar@gmail.com>
2022-04-30 07:07:39 +0700

    HTTP Post Requests

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ db: List[User] = [
     User(
         id=UUID("9e72bccb-6e65-4342-bfa9-f18046abfed8"),
         first_name="Fahmi",
-        last_name="Fakbar",
+        last_name="Akbar",
         gender=Gender.male,
         roles=[Role.student]
     ),
@@ -24,10 +24,16 @@ db: List[User] = [
 @app.get("/")
 async def root():
     return {
-        "Hello" : "World"
+        "Hello": "World"
     }
 
 @app.get("/api/v1/users")
 async def fetch_users():
-    return db;
+    return db
 
+@app.post("/api/v1/users")
+async def register_user(user: User):
+    db.append(user)
+    return {
+        "id": user.id
+    }
